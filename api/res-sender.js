43850cc5feb641192ae5a3d8b42599e9f804864a
eduardoapech <eduardo.augusto.pech97@gmail.com>
2025-07-13 21:11:49 -0300

    versao2

--- api/res-sender.js
+++ api/res-sender.js
@@ -1,13 +1,8 @@
-const axios = require('axios');
-
-export default async function handler(req, res) {
-  try {
-    const response = await axios.get('https://shinko-to-kuma.com/scripts/res-senderV2.js');
-    res.setHeader('Content-Type', 'application/javascript');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.status(200).send(response.data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Erro ao buscar o script.');
-  }
+export default function handler(req, res) {
+  res.setHeader('Content-Type', 'application/javascript');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.send(`(function () {
+  console.log("res-senderV2.js carregado com sucesso via Vercel!");
+  // Aqui estaria todo o conteúdo original do res-senderV2.js
+})();`);
 }
